36bb7b175d534c99f9ae61d9edf869405828ae4c
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-09 12:18:42 +0600

    bind, call, apply

--- object Mastering/object.js
+++ object Mastering/object.js
@@ -5,11 +5,35 @@ const normalPerson = {
     getFullName: function(){
         return this.firstName , this.lastName
     },
-    chargeBill: function(amount){
-        this.salary = this.salary - amount;
+    chargeBill: function(amount, tips, tax){
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
 
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'Rubel',
+    salary: 25000,
+}
+
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName: 'Salam',
+    salary: 28000,
+}
 normalPerson.chargeBill(150);
-console.log(normalPerson.salary);
+
+// const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroChargeBill(2000);
+
+normalPerson.chargeBill.call(heroPerson, 900, 500, 100);
+
+normalPerson.chargeBill.apply(heroPerson, [1000, 20, 50])
+
+
+
+
+console.log(heroPerson.salary);
+
+
